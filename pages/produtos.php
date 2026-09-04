4a08aba830fa9f34df0fc109bfab995ece55089f
Enzo De Sousa Batista <20241420016@ifsmp.pas.ifsuldeminas.edu.br>
2026-09-04 09:26:43 -0300

    continuaçao pagina produtos (aula elissa 04/09)

--- pages/produtos.php
+++ pages/produtos.php
@@ -6,18 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../styles/styleGeral.css">
+    <style>
+        /* Estilo para a caixa geral não ficar grudada nos cantos do monitor */
+        .container-titulo-linhas {
+            width: 71.8dvw;
+            /* Controla o tamanho máximo das linhas */
+            margin: 0 auto;
+            /* Centraliza na tela */
+        }
+
+        /* A mágica da linha */
+        .linha-lateral {
+            height: 1px;
+            /* Espessura da linha */
+            background-color: #1a233a;
+            /* Cor da linha (azul escuro igual da foto) */
+        }
+
+        /* Estilo do título principal */
+        .titulo-principal {
+            color: #1a233a;
+            /* Azul escuro */
+            font-weight: 500;
+            /* Deixa a fonte média */
+            letter-spacing: 2px;
+            /* Espaço elegante entre as letras */
+            text-transform: uppercase;
+        }
+
+        /* Estilo do subtítulo */
+        .subtitulo {
+            color: #6c757d;
+            /* Cinza padrão */
+            font-size: 0.85rem;
+        }
+    </style>
 </head>
 
-<body style="background-color: lightpink;">
-   
-     <?php include '../components/header.php'?>
-     <?php include '../components/produtoSections/corretivo.php'?>
-     <?php include '../components/produtoSections/batom-gloss.php'?>
-     <?php include '../components/produtoSections/rimel.php'?>
-     <?php include '../components/produtoSections/po-facial.php'?>
-     <?php include '../components/produtoSections/blush.php'?>
-     <?php include '../components/footer.php'?>
+<body>
+
+    <?php include '../components/header.php' ?>
+    <?php include '../components/produtoSections/banner.php' ?>
+    <?php include '../components/produtoSections/corretivo.php' ?>
+    <?php include '../components/produtoSections/batom-gloss.php' ?>
+    <?php include '../components/produtoSections/rimel.php' ?>
+    <?php include '../components/produtoSections/po-facial.php' ?>
+    <?php include '../components/produtoSections/blush.php' ?>
+    <?php include '../components/footer.php' ?>
 
 
 
